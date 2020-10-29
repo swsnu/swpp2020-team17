583b3303b31f1ec85b0f9d640d9c06b8b76acd25
thdduddn96 <thdduddn96@snu.ac.kr>
2020-10-29 21:04:19 +0900

    connect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { ConnectedRouter } from "connected-react-router";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import * as API from "./store/api"
+
+import Login from "./containers/login/login";
+
+class App extends Component {
+  render() {
+    return (
+      <ConnectedRouter history={this.props.history}>
+        <div className="App">
+          <Switch>
+            <Route path="/login/" exact component={Login} />
+            <Redirect from="/"  to={this.props.currentUser.logged_in ? "/articles/" : "/login/"} />
+          </Switch>
+        </div>
+      </ConnectedRouter>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.info.currentUser,
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
