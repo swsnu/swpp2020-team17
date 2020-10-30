ae3428557bd5017f8a94b82b2d9b784bcb3ece92
HEL <hel@HELui-MacBookPro.local>
2020-10-30 23:12:45 +0900

    remove store directory

--- frontend/src/store/actionTypes.js
+++
@@ -1,7 +0,0 @@
-export const GetLoginInfo = "GetLoginInfo";
-export const Login = "Login";
-export const Logout = "Logout"
-export const GetUsers = "GetUsers"
-export const GetPost = "GetPost"
-export const GetMainPost = "GetMainPost"
-export const PostMainPost = "PostMainPost"
