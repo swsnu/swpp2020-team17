f8cb1eefc3192a9fd67adcbcf01be808f52070b5
thdduddn96 <thdduddn96@snu.ac.kr>
2020-11-06 16:36:38 +0900

    master

--- frontend/src/store/actions/user.js
+++ frontend/src/store/actions/user.js
@@ -1,9 +1,6 @@
 import * as actionTypes from './actionTypes';
 import { push } from 'connected-react-router';
 import axios from 'axios';
-<<<<<<< HEAD
-import { push } from 'connected-react-router';
-=======
 
 axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'x-csrftoken'
@@ -22,7 +19,6 @@ function getCookie(name) {
     } 
 }
 const csrftoken = getCookie('csrftoken');
->>>>>>> login
 
 axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'X-CSRFToken'
@@ -33,31 +29,12 @@ export const login_ = (user) => {
   
 export const login = () => {
     return dispatch => {
-<<<<<<< HEAD
-        // return axios.post('/api/login/')
-        //     .then(res => {
-        //         dispatch(login_(res.data))
-        //     });
-        return fetch('/api/login/', {
-            credentials: 'include',
-            method: 'POST',
-            mode: 'same-origin',
-            headers: {
-                'Accept' : 'application/json',
-                'Content-Type': 'application/json',
-                'X-CSRFToken': csrftoken                
-            }
-        }).then(function(res) {
-            dispatch(login_(res.data))
-        })
-=======
         return axios.get('https://discord.com/api/oauth2/authorize?client_id=771395876442734603&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&response_type=code&scope=identify',
         
         ).then(res => {
             dispatch(login_(res.data))
             dispatch(push('/'))
         });
->>>>>>> login
     }
 }
 
@@ -65,30 +42,6 @@ export const sendShallWe = () => {
     return
 }
 
-<<<<<<< HEAD
-=======
-export const increaseLike = () => {
-    return dispatch => {
-        return axios.get
-    }
-}
-
-export const getComments = (postID) => {
-    return dispatch => {
-        return axios.get('/api/comment/')
-        .then((res) => dispatch(getComments_(postID, res.data)))
-    }
-}
-
-const getComments_ = (postID, comment) => {
-    let postComment = comment.filter(c => c.post_ID === postID)
-    return {
-        type: actionTypes.GetComments,
-        comments: postComment
-    }
-}
-
->>>>>>> login
 export const getUserPage = (id) => {
     return dispatch => {
         return axios.get('/api/user/' + id + '/')
@@ -102,120 +55,6 @@ const getUserPage_ = (user) => {
     return {
         type: actionTypes.GetUserPage,
         user: user
-    }
-}
-
-export const createPost = (post) => {
-    return dispatch => {
-        return axios.post('/api/post/', post)
-        .then(res => dispatch(createPost_(res.data)))
-    }
-}
-
-const createPost_ = (post) => {
-    return {
-        type: actionTypes.CreatePost,
-        post: post
-    }
-}
-
-export const getChatRoomList = () => {
-    return dispatch => {
-        return axios.get('/api/chatroom/')
-        .then(res => {
-            dispatch(getChatRoomList_(res.data))
-        })
-    }
-}
-
-<<<<<<< HEAD
-const getUserList_ = (users) => {
-    return {
-        type: actionTypes.GetUserList,
-        users: users
-    }
-}
-
-export const addTag = () => {
-    return
-}
-
-export const followUser = () => {
-    return
-}
-
-export const applySetting = () => {
-    return
-}
-
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';'); 
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break; 
-            }
-        }
-        return cookieValue; 
-    }
-}
-
-const csrftoken = getCookie('csrftoken')
-=======
-const getChatRoomList_ = (chatRoomList) => {
-    return {
-        type: actionTypes.GetChatRoomList,
-        chatRoomList: chatRoomList
-    }
-}
-
-export const createChatRoom = (chatRoom) => {
-    return dispatch => {
-        return axios.post('/api/chatroom/', chatRoom)
-        .then(res => {
-            dispatch(res => dispatch(createchatRoom_(res.data)))
-        })
-    }
-}
-
-const createchatRoom_ = (chatRoom) => {
-    return {
-        type: actionTypes.CreateChatRoom,
-        chatRoom: chatRoom
-    }
-}
-
-export const joinChatRoom = (chatRoomID) => {
-    return dispatch => {
-        return axios.get('/api/chatroom/' + chatRoomID + '/')
-        .then(res => {
-            dispatch(res => dispatch(joinChatRoom_(res.data)))
-        })
-    }
-}
-
-const joinChatRoom_ = (chatRoom) => {
-    return {
-        type: actionTypes.JoinChatRoom,
-        chatRoom: chatRoom
-    }
-}
-
-export const exitChatRoom = (chatRoomID) => {
-    return dispatch => {
-        return axios.get('/api/chatroom' + chatRoomID + '/')
-        .then(res => {
-            dispatch(res => dispatch(exitChatRoom_(res.data)))
-        })
-    }
-}
-
-const exitChatRoom_ = (chatRoom) => {
-    return {
-        type: actionTypes.ExitChatRoom
     }
 }
 
@@ -263,18 +102,5 @@ export const applySetting = () => {
     return
 }
 
-export const getGridPost = () => {
-    return
-}
-
-export const putPost = () => {
-    return
-}
-
-export const deletePost = () => {
-    return
-}
 
 
-
->>>>>>> login
