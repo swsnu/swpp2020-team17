3da354634b8fc72881646bdfedcf97b48b91e991
moovinzoo <moovinzoo@gmail.com>
2020-11-29 13:40:13 +0900

    [Update] FriendList featured

--- frontend/src/containers/MyPage/MyPage.js
+++ frontend/src/containers/MyPage/MyPage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actionCreators from '../../store/actions/index';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import Post from '../../containers/Post/Post';
-import { Divider } from 'antd';
+import { Divider, List, Button, Space } from 'antd';
 import Profile from '../../components/Profile/Profile';
+import Author from '../../components/Author/Author';
 
 const MyPageContainer = styled.div`
     display: flex;
@@ -17,6 +18,7 @@ const MyPageContainer = styled.div`
     height: 100%;
     margin: 0;
     justify-content: space-between;
+    flex-wrap: wrap;
 `;
 
 const MyPageLeftContainer = styled.div`
@@ -37,6 +39,53 @@ const ProfileCardWrapper = styled.div`
 const FriendListWrapper = styled.div`
     height: 100%;
     box-shadow: 3px 3px 5px 2px rgba(0,0,0,0.1);
+`;
+
+const FriendItemWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+`;
+
+const AuthorItem = styled.div`
+    flex: none; // flex-grow/shrink/basis : 0/0/auto
+`;
+
+const SpaceBetweenItem = styled(Space)`
+    flex-grow: 1;
+    flex-shrink: 1;
+`;
+
+const ButtonItem = styled.div`
+    flex: none;
+    ${(props) => props.active && `
+    animation: ${buttonShake} .8s ;
+    `}
+`;
+
+// FIXME: 애니메이션 작동 안됨.
+const buttonShake = keyframes`
+    0%{
+        transform: translate(0);
+    }
+    20%,
+    40%,
+    60%,
+    80%{
+        transform: translate(.8em);
+        box-shadow: 0px 6px 20px rgba(0, 0, 0, 0.5);
+    }
+    10%,
+    30%,
+    50%{
+        transform: translate(-.2em);
+    }
+    70%,
+    100%{
+        transform: translate(0);
+    }
 `;
 
 const MyPageRightContainer = styled.div`
@@ -120,6 +169,16 @@ class MyPage extends Component {
         this.props.changeTagState()
     }
 
+    //TODO:
+    handleAuthorClicked() {
+
+    }
+
+    //TODO:
+    handleShallWeClicked() {
+
+    }
+
     render() {
         // let name = this.props.storedCurrentUser.username;
         // let avatar = this.props.storedCurrentUser.avatar;
@@ -127,13 +186,52 @@ class MyPage extends Component {
         // let friendList = this.props.storedCurrentUser.friendList;
         let user = this.props.storedCurrentUser;
         let userProfile = (user !== null)?
-            (<Profile 
+            (<Profile
                 name={user.username}
                 avatar={user.avatar}
                 tagList={user.tagList}
             />):
             (<p>Please login!</p>)
         ;
+        //FIXME: 더미로 테스트하다가 friend 추가 잘 되면, 바꿔놓기.
+        // let friendList = this.props.storedCurrentUser.friendList;
+        let friendList = [];
+        friendList.push({
+            "id": 4,
+            "username": "Song1",
+            "login": true,
+            "avatar": "https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg",
+            "chatroom": -1,
+            "friendList": [],
+            "postList": [6, 8],
+            "shallWeRoomList": [],
+            "watchedPostList": [],
+            "tagList": []
+        })
+        friendList.push({
+            "id": 5,
+            "username": "Lee1",
+            "login": true,
+            "avatar": null,
+            "chatroom": -1,
+            "friendList": [],
+            "postList": [7, 9, 10],
+            "shallWeRoomList": [],
+            "watchedPostList": [],
+            "tagList": []
+        })
+        friendList.push({
+            "id": 6,
+            "username": "\uc774\ub3d9\uc8fc",
+            "login": true,
+            "avatar": null,
+            "chatroom": -1,
+            "friendList": [],
+            "postList": [],
+            "shallWeRoomList": [],
+            "watchedPostList": [],
+            "tagList": []
+        })
 
         return(
             <MyPageContainer>
@@ -142,14 +240,45 @@ class MyPage extends Component {
                         {userProfile}
                     </ProfileCardWrapper>
                     <FriendListWrapper>
-                        <Divider orientation="left" style={{ marginTop: 0 }}>
+                        <Divider orientation="center" style={{ marginTop: 0 }}>
                             Friends
                         </Divider>
+                        <List
+                            dataSource={friendList}
+                            renderItem={item => (
+                                <List.Item key={item.id}>
+                                        <FriendItemWrapper>
+                                            <AuthorItem onClick={this.handleAuthorClicked} style={{ cursor: "pointer" }} >
+                                                <Author
+                                                    //FIXME: user로 넘기도록 수정해야함
+                                                    name={item.username}
+                                                    avatar={item.avatar}
+                                                    showOnline={true}
+                                                />
+                                                </AuthorItem>
+                                            <SpaceBetweenItem />
+                                            <ButtonItem>
+                                                <Button
+                                                    type="primary"
+                                                    shape="round"
+                                                    onClick={this.handleShallWeClicked}
+                                                    disabled="true"
+                                                    // type="primary"
+                                                    size="small"
+                                                    style={{ fontSize: 10, fontWeight: "bolder" }}
+                                                >
+                                                    Shall We
+                                                </Button>
+                                            </ButtonItem>
+                                        </FriendItemWrapper>
+                                </List.Item>
+                            )}
+                        />
                     </FriendListWrapper>
                 </MyPageLeftContainer>
                 <MyPageRightContainer>
                     <GridPostsWrapper>
-                        <Divider orientation="left" style={{ marginTop: 0 }}>
+                        <Divider orientation="center" style={{ marginTop: 0 }}>
                             Gallery
                         </Divider>
                     </GridPostsWrapper>
