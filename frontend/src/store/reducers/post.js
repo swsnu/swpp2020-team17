97f8c9b97a3340e91051343a489d1cef6c66586b
moovinzoo <moovinzoo@gmail.com>
2020-12-04 07:07:29 +0900

    [Error] Fixing Comment feature

--- frontend/src/store/reducers/post.js
+++ frontend/src/store/reducers/post.js
@@ -1,3 +1,40 @@
+// import * as actionTypes from '../actions/actionTypes';
+
+
+// const reducer = (state = {
+//     selectedPost: null,
+//     postList: [],
+//     commentList: [],
+// }, action) => {
+//     switch (action.type) {
+//         case actionTypes.GetPostList:
+//             return { ...state, postList: action.posts };
+//         case actionTypes.CreatePost:
+//             return { ...state, postList: [...state.postList.concat(action.post)], selectedPost: action.post };
+//         case actionTypes.GetPost:
+//             return { ...state, selectedPost: action.post };
+//         case actionTypes.PutPost:
+//             const modifiedPosts = state.postList.map(post => {
+//                 if (post.id === action.post.id) {
+//                     return { ...action.post };
+//                 } else return { ...post };
+//             })
+//             return { ...state, postList: modifiedPosts };
+//         case actionTypes.DeletePost:
+//             const deletedPosts = state.postList.filter(post => {
+//                 return post.id !== action.post.id;
+//             });
+//             return { ...state, postList: deletedPosts };
+//         case actionTypes.GetCommentList:
+//             return { ...state, commentList: action.commentList }
+//         default:
+//             break;
+//     }
+//     return state
+// };
+
+// export default reducer;
+
 import * as actionTypes from '../actions/actionTypes';
 
 
@@ -16,7 +53,7 @@ const reducer = (state = {
         case actionTypes.PutPost:
             const modifiedPosts = state.postList.map(post => {
                 if (post.id === action.post.id) {
-                  return { ...action.post };
+                    return { ...action.post };
                 } else return { ...post };
             })
             return { ...state, postList: modifiedPosts };
@@ -24,7 +61,7 @@ const reducer = (state = {
             const deletedPosts = state.postList.filter(post => {
                 return post.id !== action.post.id;
             });
-            return { ...state, postList: deletedPosts};
+            return { ...state, postList: deletedPosts };
         case actionTypes.GetCommentList:
             return { ...state, commentList: action.commentList }
         default:
